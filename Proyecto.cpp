9a9ff207dcdaa7177fe159c4e5d87df3468511da
Gabriel <gabrieldn2412@gmail.com>
2020-08-03 17:59:12 -0300

    Agregue nueva libreria, Agregue Codigo del juego 1, Quite printf en el primer case 1.
    Agrege una funcion del juego MayMen y lo agregue al case 1

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "Metodos.h"
 
+void MayMen();
 
 main()
 {
@@ -13,7 +15,7 @@ main()
 	switch(opcion_final)
 	{
 		case 1:
-			printf("Este juego consiste en adivinar un número que está entre 1 y 999 denominado número secreto");
+			MayMen();
 			break;
 		case 2:
 			printf("\nEs un juego en el que se tiene que adivinar un número de 4 cifras, es decir entre 1000 y 9999");
@@ -36,6 +38,112 @@ main()
 		system("pause");
 	}while(opcion_final!=5);	
 }
+
+
+void MayMen()
+{
+	int numero_aleatorio=0,intentos=11, numero_ingresado, numero_mayor, numero_menor;
+	printf("\n--Este juego consiste en adivinar un numero que esta entre 1 y 999 denominado numero secreto--");
+	printf("\n1. Itentos para encontrar el numero: 10");
+	printf("\n2. El puntaje obtenido depende de la cantidad de intentos");
+	printf("\n3. Al perder o ganar el juego se mostrara el numero secreto");
+	printf("\n4. Jugar");
+	
+	srand(time(NULL));
+	numero_aleatorio=1+rand()%(999);
+	
+	do
+	{
+		printf("\n\nIngrese un numero: ");
+		scanf("%d",&numero_ingresado);
+		if(numero_ingresado>numero_aleatorio)
+		{
+			printf("\nEl numero ingresado es mayor al aleatorio");
+		}
+		if(numero_ingresado<numero_aleatorio)
+		{
+			printf("\nEl numero ingresado es menor al aleatorio");
+		}
+		if(numero_ingresado==numero_aleatorio)
+		{
+			printf("\nEl numero ingresado es igual al aleatorio");
+		}
+			
+	intentos=intentos-1;	
+	}while(numero_ingresado==numero_aleatorio and intentos!=0);
+	
+	break;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
